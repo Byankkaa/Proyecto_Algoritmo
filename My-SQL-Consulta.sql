3fab595672b0d282cd41657fef7865975a6e3c60
Bri <briancruz.et32@gmail.com>
2025-11-11 20:11:08 -0300

    Consultas
    Consultas

---
+++ My-SQL-Consulta.sql
@@ -0,0 +1,228 @@
+DROP DATABASE IF EXISTS canchas_futbol;
+CREATE DATABASE IF NOT EXISTS canchas_futbol;
+USE canchas_futbol;
+
+CREATE TABLE Clientes(
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    dni VARCHAR(8) NOT NULL UNIQUE,
+    nombre VARCHAR(100) NOT NULL,
+    telefono INT(10)
+);
+
+CREATE TABLE Canchas(
+    id_cancha INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(50) NOT NULL,
+    precio_hora DECIMAL(10,2) NOT NULL,
+    estado ENUM('Disponible','Ocupada','Mantenimiento') DEFAULT 'Disponible'
+);
+
+CREATE TABLE Horarios(
+    id_horario INT AUTO_INCREMENT PRIMARY KEY,
+    hora_inicio TIME NOT NULL,
+    hora_fin TIME NOT NULL,
+    tipo_turno ENUM('Mañana','Tarde','Noche'),
+    dia_semana ENUM('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo')
+);
+DROP DATABASE IF EXISTS canchas_futbol;
+CREATE DATABASE IF NOT EXISTS canchas_futbol;
+USE canchas_futbol;
+
+CREATE TABLE Clientes(
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    dni VARCHAR(8) NOT NULL UNIQUE,
+    nombre VARCHAR(100) NOT NULL,
+    telefono INT(10)
+);
+
+CREATE TABLE Canchas(
+    id_cancha INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(50) NOT NULL,
+    precio_hora DECIMAL(10,2) NOT NULL,
+    estado ENUM('Disponible','Ocupada','Mantenimiento') DEFAULT 'Disponible'
+);
+
+CREATE TABLE Horarios(
+    id_horario INT AUTO_INCREMENT PRIMARY KEY,
+    hora_inicio TIME NOT NULL,
+    hora_fin TIME NOT NULL,
+    tipo_turno ENUM('Mañana','Tarde','Noche'),
+    dia_semana ENUM('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo')
+);
+
+CREATE TABLE Reservas(
+    id_reserva INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    id_cancha INT NOT NULL,
+    id_horario INT NOT NULL,
+    fecha_reserva DATE NOT NULL,
+    estado_pago ENUM('Pendiente','Pagado','Cancelado') DEFAULT 'Pendiente',
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
+    FOREIGN KEY (id_cancha) REFERENCES Canchas(id_cancha),
+    FOREIGN KEY (id_horario) REFERENCES Horarios(id_horario)
+);
+
+CREATE TABLE Pagos(
+    id_pago INT AUTO_INCREMENT PRIMARY KEY,
+    id_reserva INT NOT NULL,
+    monto DECIMAL(10,2) NOT NULL,
+    metodo_pago ENUM('Efectivo','Tarjeta','Transferencia') NOT NULL,
+    fecha_pago DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_reserva) REFERENCES Reservas(id_reserva)
+);
+
+CREATE TABLE Facturas(
+    id_factura INT AUTO_INCREMENT PRIMARY KEY,
+    id_pago INT NOT NULL,
+    fecha_emision DATE NOT NULL,
+    total DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (id_pago) REFERENCES Pagos(id_pago)
+);
+
+CREATE TABLE Mantenimiento_Canchas(
+    id_mantenimiento INT AUTO_INCREMENT PRIMARY KEY,
+    id_cancha INT NOT NULL,
+    tipo_mantenimiento VARCHAR(100) NOT NULL,
+    costo DECIMAL(10,2),
+    estado ENUM('Pendiente','Completado','Finalizado') DEFAULT 'Pendiente',
+    fecha DATE NOT NULL,
+    FOREIGN KEY (id_cancha) REFERENCES Canchas(id_cancha)
+);
+
+CREATE TABLE Cancheros(
+    id_canchero INT AUTO_INCREMENT PRIMARY KEY,
+    dni VARCHAR(8) NOT NULL UNIQUE,
+    nombre VARCHAR(100) NOT NULL,
+    telefono INT(10),
+    horario VARCHAR(50),
+    sueldo DECIMAL(10,2)
+);
+
+INSERT INTO Canchas (tipo, precio_hora, estado)
+VALUES
+('Fútbol 5', 35000, 'Disponible'),
+('Fútbol 8', 80000, 'Disponible'),
+('Fútbol 11', 150000, 'Mantenimiento'),
+('Fútbol 7',60000, 'Disponible'),
+('Fútbol 9',100000,'Ocupada');
+
+INSERT INTO Cancheros (nombre, telefono, dni, sueldo)
+ VALUES
+('Brian Cruz', '1134567890', '30111222', 350000),
+('Martina Orlandi', '1122233344', '28455679', 370000),
+('Alvin Viana','1167893210','29566789',360000),
+('Luna Viana','1145687721','31222333',355000),
+('Juan Peron','1144890092','29999888', 365000);
+
+INSERT INTO Clientes (nombre, telefono, dni)
+VALUES
+('Thiago Viana', '1122454312', '40865431'),
+('Daniela Mansilla', '11875432', '40986722'),
+('Maximiliano Viera', '11777332', '40232665'),
+('Francisco Gutierrez', '11088654', '40877682'),
+('Faustino Sarmiento','1176543210','40788900');
+
+INSERT INTO Horarios (hora_inicio, hora_fin, tipo_turno, dia_semana)
+VALUES
+('10:00', '11:00', 'Mañana', 'Lunes'),
+('18:00', '19:00', 'Tarde', 'Viernes'),
+('21:00', '22:00', 'Noche', 'Sabado'),
+('15:00', '16:00', 'Tarde', 'Domingo'),
+('09:00', '10:00', 'Mañana', 'Martes');
+
+INSERT INTO Reservas (id_cliente, id_cancha, id_horario, fecha_reserva, estado_pago)
+VALUES
+(1, 1, 1, '2025-10-20', 'Pagado'),
+(2, 2, 2, '2025-10-21', 'Pendiente'),
+(3, 3, 3, '2025-11-04', 'Pendiente'),
+(4, 1, 4, '2025-10-30', 'Pagado'),
+(5, 2, 5, '2025-10-25', 'Pendiente');
+
+INSERT INTO Pagos (id_reserva, monto, metodo_pago)
+VALUES
+(1, 35000, 'Transferencia'),
+(2, 80000, 'Tarjeta'),
+(3, 150000, 'Transferencia'),
+(4, 60000, 'Efectivo'),
+(5, 100000, 'Tarjeta');
+
+INSERT INTO Facturas (id_pago, fecha_emision, total)
+ VALUES
+(1, '2025-10-20', 35000),
+(2, '2025-10-30', 80000),
+(3, '2025-11-04', 150000),
+(4, '2025-10-30', 60000),
+(5, '2025-10-25', 100000);
+
+INSERT INTO Mantenimiento_Canchas (id_cancha, tipo_mantenimiento, costo, estado, fecha)
+VALUES
+(2, 'Cambio de césped', 30000, 'Finalizado', '2025-09-10'),
+(3, 'Revisión de luces', 20000, 'Pendiente', '2025-09-25'),
+(1, 'Limpieza General', 5000, 'Finalizado', '2025-09-25'),
+(2, 'Cambio de red', 8000, 'Finalizado', '2025-09-15'),
+(5, 'Cambio de reflectores', 12000, 'Pendiente', '2025-10-02');
+
+/*----------- Muestra todas las reservas junto al cliente, fecha y hora; mas el estado del pago ----------*/
+SELECT
+  r.id_reserva, c.nombre AS cliente, ca.tipo AS cancha, h.hora_inicio, h.hora_fin, h.dia_semana,
+  r.fecha_reserva, r.estado_pago
+FROM Reservas r
+Inner JOIN Clientes c ON r.id_cliente = c.id_cliente
+Inner JOIN Canchas ca ON r.id_cancha = ca.id_cancha
+Inner JOIN Horarios h ON r.id_horario = h.id_horario;
+
+
+/*----------- Muestra los ingresos totales de los distintos tipos de cancha ----------*/
+SELECT
+  ca.tipo AS cancha,
+  SUM(p.monto) AS ingresos_totales
+FROM Pagos p
+Inner JOIN Reservas r ON p.id_reserva = r.id_reserva
+Inner JOIN Canchas ca ON r.id_cancha = ca.id_cancha
+GROUP BY ca.tipo;
+
+
+/*----------- Muestra el tipo y el precio por horas de las canchas que estén disponibles ----------*/
+SELECT tipo, precio_hora
+FROM Canchas
+WHERE estado = 'Disponible';
+
+
+
+/*----------- Muestra las reservas, el cliente, el tipo de cancha y la fecha donde el estado del pago esté aún pendiente ----------*/
+SELECT
+  r.id_reserva, c.nombre AS cliente, ca.tipo AS cancha, r.fecha_reserva
+FROM Reservas r
+Inner JOIN Clientes c ON r.id_cliente = c.id_cliente
+Inner JOIN Canchas ca ON r.id_cancha = ca.id_cancha
+WHERE r.estado_pago = 'Pendiente';
+
+
+/*----------- Muestra el tipo de cancha, el tipo de mantenimiento, el costo, el estado y la fecha de las canchas según la fecha en orden descendiente. ----------*/
+SELECT
+  ca.tipo AS cancha, m.tipo_mantenimiento, m.costo, m.estado, m.fecha
+FROM Mantenimiento_Canchas m
+Inner JOIN Canchas ca ON m.id_cancha = ca.id_cancha
+ORDER BY m.fecha DESC;
+
+/*----------- Listado de todas las canchas con tipo, precio por hora y estado ----------*/
+SELECT tipo, precio_hora, estado FROM canchas;
+
+/*----------- Mostrame el DNI, nombre y teléfono de todos los clientes ----------*/
+SELECT dni, nombre, telefono FROM Clientes;
+
+/*----------- Mostrame cada cancha con la cantidad de mantenimientos hechos, el costo total que se gastó en ellos y el promedio por mantenimiento ----------*/
+SELECT c.id_cancha, c.tipo, COUNT(m.id_cancha) as cantidadMantenimientos, SUM(m.costo) as costoTotal, AVG(m.costo) as promedioPorMantenimiento FROM mantenimiento_canchas m
+JOIN canchas c on m.id_cancha = c.id_cancha
+GROUP BY m.id_cancha;
+
+
+/*----------- Mostrame cuantos pagos se hiceron con cada metodo y el total recaudado ----------*/
+SELECT metodo_pago, count(metodo_pago) as cantidadUsado, SUM(monto) as totalRecaudado from Pagos
+GROUP BY metodo_pago;
+
+/*----------- Mostrame que canchas se usaron el viernes, con el cliente y la hora que reservo ----------*/
+SELECT c.id_cancha, cl.nombre as Cliente, h.hora_inicio, h.hora_fin FROM Reservas r
+JOIN canchas c on r.id_cancha = c.id_cancha
+JOIN clientes cl on cl.id_cliente = r.id_cliente
+JOIN horarios h on h.id_horario = r.id_horario
+WHERE h.dia_semana = 'Viernes';
